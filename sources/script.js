76ec8914e0b5b21ddf13f76dbf20273bf26340a4
joe-toni <joefaburrieta@gmail.com>
2021-11-01 13:44:25 -0700

    Added in dynamic search history display and intitial setup conditions so that a city is always displayed

--- sources/script.js
+++ sources/script.js
@@ -1,137 +1,232 @@
 //Establishing Global Variables and References
     var APIkey = "cd0c34537ffad5abb85b20f6c5cdf3cc";
     //This array saves the moment objects that will be used to display later on
-    const calendarDates =
-    [
-    moment().format("(M/D/YYYY)"),
-    moment().add(1, 'days').format("M/D/YYYY"),
-    moment().add(2, 'days').format("M/D/YYYY"),
-    moment().add(3, 'days').format("M/D/YYYY"),
-    moment().add(4, 'days').format("M/D/YYYY"),
-    moment().add(5, 'days').format("M/D/YYYY")
-    ];
+     const calendarDates =
+     [
+     moment().format("(M/D/YYYY)"),
+     moment().add(1, 'days').format("M/D/YYYY"),
+     moment().add(2, 'days').format("M/D/YYYY"),
+     moment().add(3, 'days').format("M/D/YYYY"),
+     moment().add(4, 'days').format("M/D/YYYY"),
+     moment().add(5, 'days').format("M/D/YYYY")
+     ];
     //
 
     //This area query selects all the necessary elments from the document and converts them
     // into jquery objects for easier manipulation.
-    const history = document.querySelectorAll(".slot");
-    const jqHistory = [];
-    history.forEach(element => jqHistory.push($(element)));
+        const jqHistory = $(".searchHistory");
+       // const jqHistory = [];
+       // history.forEach(element => jqHistory.push($(element)));
 
-    const jqToday = $(".searchResults");
+        const jqToday = $(".searchResults");
 
-    const days = document.querySelectorAll(".dayDisplay");
-    const jqDays = [];
-    days.forEach(element => jqDays.push($(element)));
+        const days = document.querySelectorAll(".dayDisplay");
+        const jqDays = [];
+        days.forEach(element => jqDays.push($(element)));
     //
 
     //This portion initiallizes an object that will hold all the neccisary values to be displayed for our city
-    const currentCity =
-    {
-        name: "",
-        date: calendarDates[0],
-        icon: "",
-        temperature: "",
-        wind: "",
-        humidity: "",
-        uvIndex: ""
-    }
+     const currentCity =
+     {
+         name: "",
+         date: calendarDates[0],
+         icon: "",
+         temperature: "",
+         wind: "",
+         humidity: "",
+         uvIndex: ""
+     }
     //
 
     //This is an array of objects that will hold the values for each upcomming forcast day with each 
     //individual object represening one day
-    const forcast =
-        [
-        {
-            date:calendarDates[1],
-            icon: "",
-            temperature: "",
-            wind: "",
-            humidity: ""
-        },
-        {
-            date:calendarDates[2],
-            icon: "",
-            temperature: "",
-            wind: "",
-            humidity: ""
-        },
-        {
-            date:calendarDates[3],
-            icon: "",
-            temperature: "",
-            wind: "",
-            humidity: ""
-        },
-        {
-            date:calendarDates[4],
-            icon: "",
-            temperature: "",
-            wind: "",
-            humidity: ""
-        },
-        {
-            date:calendarDates[5],
-            icon: "",
-            temperature: "",
-            wind: "",
-            humidity: ""
-        }
-        ]
+     const forcast =
+         [
+         {
+             date:calendarDates[1],
+             icon: "",
+             temperature: "",
+             wind: "",
+             humidity: ""
+         },
+         {
+             date:calendarDates[2],
+             icon: "",
+             temperature: "",
+             wind: "",
+             humidity: ""
+         },
+         {
+             date:calendarDates[3],
+             icon: "",
+             temperature: "",
+             wind: "",
+             humidity: ""
+         },
+         {
+             date:calendarDates[4],
+             icon: "",
+             temperature: "",
+             wind: "",
+             humidity: ""
+         },
+         {
+             date:calendarDates[5],
+             icon: "",
+             temperature: "",
+             wind: "",
+             humidity: ""
+         }
+         ]
     //
 //spacing
    
 //This is a place holder variable used to test the information retrieved by the API
-var city = "los angeles";
-getTheWeather();
+var city = "";
+var searchedCities = [];
 
-//This is the main function and first aquires the coordinates of the searched city from an initial API call then uses that same information
-//to make a new API call for the correct forcast information it then populates the global objects with the necessary information after the 
-//data has been retrieved and uses the objects to run the display functions. It also catches if the given input does not result in a valid input
-function getTheWeather()
+const userEntry = $(".searchInput");
+const cityInput = $(".searchField");
+console.log(cityInput.val());
+userEntry.on("submit", function()
 {
-    let lon = "";
-    let lat = "";
+    event.preventDefault();
+    city = cityInput.val();
+    cityInput.val("");
+    getTheWeather();
+});
 
-    var firstQueryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIkey;
-    fetch(firstQueryURL)
-    .then(function(response){return response.json();})
-    .then(function(data)
+initialSetup();
+startingPageSetup();
+
+//This function checks to see if a local storage item exists setting one up if not and simply
+//pulling and populating the information if  it does.
+function initialSetup()
+{
+    if(localStorage.getItem("searchHistory")===null)
     {
-        currentCity.name = data.name;   
-        lon = data.coord.lon;
-        lat = data.coord.lat
-        
-        let secondQueryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=imperial&appid=${APIkey}`;
-        fetch(secondQueryURL)
-        .then(function(response){return response.json();})
-        .then(function(data)
-        {
-            currentCity.icon = data.current.weather[0].icon;
-            currentCity.temperature = data.current.temp;
-            currentCity.wind = data.current.wind_speed;
-            currentCity.humidity = data.current.humidity;
-            currentCity.uvIndex = data.current.uvi;
-            displayResults(currentCity);
-
-            for(i = 0; i < 5; i++)
-            {
-                forcast[i].icon = data.daily[i].weather[0].icon;
-                forcast[i].temperature = data.daily[i].temp.day;
-                forcast[i].wind = data.daily[i].wind_speed;
-                forcast[i].humidity = data.daily[i].humidity; 
-            }
-            displayForcast(forcast);
-        })
-    })
-    .catch(function(error)
+        localStorage.setItem("searchHistory", JSON.stringify(searchedCities));
+    };
+    let storagePull = JSON.parse(localStorage.getItem("searchHistory"));
+    console.log(storagePull);
+    for( let i = 0; i < storagePull.length; i++)
     {
-        alert("Invalid Input Please try Again");
-    });
+        searchedCities.push(storagePull[i]);
+    } 
+};
 
+//This function will run an example city if it is the users first time running the site or loading
+// the most recent searched city if one is found in local storage after settup.
+function startingPageSetup()
+{
+    if(searchedCities.length === 0 )
+    {
+        city = "Atlanta";
+        getTheWeather();
+    }
+    else
+    {
+        let mostRecent = searchedCities.length-1;
+        city = searchedCities[mostRecent];
+        getTheWeather();
+    }
 }
 
-//Displaying Fetch Results
+//This function will display the search history saved in the local varaible by appending new html elements
+//directly to the existing div, after clearing the existing ones to avoid repeating lists.
+function displaySearchHistory()
+{
+    jqHistory.html("");
+    if(searchedCities.length > 0)
+    {
+       let  index = searchedCities.length - 1;
+       //let postition = 0;
+        while(index >= 0)
+        {
+            jqHistory.append(`<p class = "savedCity">${searchedCities[index]}</p>`)
+           // jqHistory[postition].html(searchedCities[index]).addClass("savedCity");
+            index--;
+           // postition++;
+        }
+    }
+};
+
+
+//Calling the API & Setting Data
+    //This is the main function and first aquires the coordinates of the searched city from an initial API call then uses that same information
+    //to make a new API call for the correct forcast information it then populates the global objects with the necessary information after the 
+    //data has been retrieved and uses the objects to run the display functions. It also catches if the given input does not result in a valid input
+        function getTheWeather()
+        {
+            let lon = "";
+            let lat = "";
+        
+            var firstQueryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIkey;
+            fetch(firstQueryURL)
+            .then(function(response){return response.json();})
+            .then(function(data)
+            {
+                currentCity.name = data.name;  
+
+                lon = data.coord.lon;
+                lat = data.coord.lat;
+
+                let secondQueryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=imperial&appid=${APIkey}`;
+                fetch(secondQueryURL)
+                .then(function(response){return response.json();})
+                .then(function(data)
+                {
+                    currentCity.icon = data.current.weather[0].icon;
+                    currentCity.temperature = data.current.temp;
+                    currentCity.wind = data.current.wind_speed;
+                    currentCity.humidity = data.current.humidity;
+                    currentCity.uvIndex = data.current.uvi;
+                    displayResults(currentCity);
+                
+                    for(i = 0; i < 5; i++)
+                    {
+                        forcast[i].icon = data.daily[i].weather[0].icon;
+                        forcast[i].temperature = data.daily[i].temp.day;
+                        forcast[i].wind = data.daily[i].wind_speed;
+                        forcast[i].humidity = data.daily[i].humidity; 
+                    }
+                    displayForcast(forcast);
+                    adjustSearchHistory();
+                })
+            })
+            .catch(function(error)
+            {
+                alert("Invalid Input Please try Again");
+            });
+        }
+//Section End
+
+//This function will check if the current searched city is already saved in the search history array pushing it to the
+//front of the list if so or simply adding it in if not.
+function adjustSearchHistory()
+{
+    if(searchedCities.includes(currentCity.name))
+    {
+        let index = searchedCities.indexOf(currentCity.name);
+        while (index != searchedCities.length-1)
+        {
+            let temporary = searchedCities[index];
+            searchedCities[index] = searchedCities[index + 1];
+            searchedCities[index + 1] = temporary;
+            index++;
+        }
+        console.log(searchedCities);
+    }
+    else
+    {
+        console.log(searchedCities);
+        searchedCities.push(currentCity.name);
+    };
+    localStorage.setItem("searchHistory", JSON.stringify(searchedCities));
+    displaySearchHistory();
+};
+
+ //Displaying Fetch Results
     //The following functions should correctly display the resulting data from our fetch request but should only
     //ever be called from inside the request to make sure the elments are only populating once the data is obtained.
       
